8066b71d1e431ecbcdaaecd4de0c309aa6a9f879
Anjasfedo <fedoaf98@gmail.com>
2024-02-11 10:51:35 +0700

    Use some Filter Parameter

--- server/api/lists.get.ts
+++ server/api/lists.get.ts
@@ -1,16 +1,22 @@
 import { getDataByPage, getTotalPage } from "../helpers/serverHelpers";
 
 export default defineEventHandler(async (event) => {
+  const { page, filter } = getQuery(event);
 
-  const { page } = getQuery(event);
+  // https://komiku-api.fly.dev/api/comic/list
+  // https://komiku-api.fly.dev/api/comic/list?filter=manga
+  // https://komiku-api.fly.dev/api/comic/list?filter=manhwa
+  // https://komiku-api.fly.dev/api/comic/list?filter=manhua
 
-  const lists = await $fetch("https://komiku-api.fly.dev/api/comic/list");
+  const lists = await $fetch(
+    `https://komiku-api.fly.dev/api/comic/list?filter=${filter}`
+  );
 
   const pageSize = 10;
 
   const result = getDataByPage(lists.data, page, pageSize);
 
-  const totalPages = getTotalPage(lists.data, pageSize)
+  const totalPages = getTotalPage(lists.data, pageSize);
 
   return { result, totalPages };
 });
